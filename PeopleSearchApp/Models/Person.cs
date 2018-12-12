ed67c407c0832fc4e501af0f7dba05b997d23d3b
justnsane10 <joshnielsen10@icloud.com>
2018-12-12 13:43:17 -0700

    Fixed XML

--- PeopleSearchApp/Models/Person.cs
+++ PeopleSearchApp/Models/Person.cs
@@ -7,41 +7,71 @@ namespace PeopleSearchApp.Models
     /// </summary>
     public class Person
     {
+        /// <summary>
+        /// Person Model
+        /// </summary>
         [Key]
         public int PersonID { get; set; }
 
+        /// <summary>
+        /// First Name of Person
+        /// </summary>
         [Required(ErrorMessage = "Must Provide a First Name")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last Name of Person
+        /// </summary>
         [Required(ErrorMessage = "Must Provide a Last Name")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Current Residential Street Address for Person
+        /// </summary>
         [Required(ErrorMessage = "Must Provide an Street Address")]
         [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
 
+        /// <summary>
+        /// City of Current Residential for Person
+        /// </summary>
         [Required(ErrorMessage = "Must Provide a City")]
         [Display(Name = "City")]
         public string City { get; set; }
 
+        /// <summary>
+        /// State of Current Residential for Person
+        /// </summary>
         [Required(ErrorMessage = "Must Provide a State")]
         [Display(Name = "State")]
         public string State { get; set; }
 
+        /// <summary>
+        /// Zip Code of Current Residential for Person
+        /// </summary>
         [Required(ErrorMessage = "Must Provide a Zip Code")]
         [Display(Name = "Zip Code")]
         public string Zip { get; set; }
 
+        /// <summary>
+        /// Age of Person
+        /// </summary>
         [Required(ErrorMessage = "Must Provide an Age")]
         [Display(Name = "Age")]
         [Range(0, 120)]
         public int Age { get; set; }
 
+        /// <summary>
+        /// Personal Interests for Person
+        /// </summary>
         [Display(Name = "Interests")]
         public string Interests { get; set; }
 
+        /// <summary>
+        /// Photo of Person
+        /// </summary>
         [Display(Name = "Photo")]
         public string PhotoPath { get; set; }
     }
--- PeopleSearchApp/Scripts/myScript.js
+++ PeopleSearchApp/Scripts/myScript.js
@@ -27,7 +27,7 @@ function clearList() {
 function fillTable(response) {
     if (response != null) {
         if ($('#personTableList').find("thead").length === 0) {
-            var table = '<thead>' +
+            var table = '<thead class="thead-dark">' +
                             '<tr>' +
                                 '<th>First Name</th>' +
                                 '<th>Last Name</th>' +
@@ -35,7 +35,7 @@ function fillTable(response) {
                                 '<th>Age</th>' +
                                 '<th>Interests</th>' +
                                 '<th>Picture</th>' +
-                                '<th>API View</th>'
+                                '<th>API View</th>' +
                             '</tr>' +
                         '/thead><tbody>';
             $('#personTableList').append(table);
@@ -48,7 +48,7 @@ function fillTable(response) {
                             '<td>' + response[i].Age + '</td>' +
                             '<td>' + response[i].Interests + '</td>' +
                             '<td><img src="\\' + response[i].PhotoPath + '" width="50"/></td>' +
-                            '<td><a href="/api/People/' + response[i].PersonID + '"> API </a></td>'
+                            '<td><a href="/api/People/' + response[i].PersonID + '"> API </a></td>' +
                        '</tr>';
             $("#personTableList").append(item);
         }
